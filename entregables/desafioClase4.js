c679eb1c68bcd03d3012182f25eb510eea390863
juanPabloSarobe <jp.sarobe@gmail.com>
2024-04-21 20:03:58 -0300

    desafio clase 4 terminado

--- entregables/desafioClase4.js
+++ entregables/desafioClase4.js
@@ -13,30 +13,42 @@
     con el objetivo de integrar todos los proyectos al final del curso.
  */
 
+import { existsSync, promises } from "fs";
+
 class ProductManager {
-  constructor() {
-    this.products = [];
+  constructor(path) {
+    this.path = path;
   }
 
-  #generateId = () => {
+  #generateId = async () => {
     let newId;
-    if (!this.products.length) {
+    const products = await this.getProducts();
+    if (!products.length) {
       newId = 1;
     } else {
-      const lastObj = this.products.length - 1;
-      newId = this.products[lastObj].id + 1;
+      const lastObj = products.length - 1;
+      newId = products[lastObj].id + 1;
     }
     return newId;
   };
 
-  #validateCode = (code) => {
-    if (this.products.length > 0) {
-      const findCode = this.products.some((elem) => elem.code === code);
-      if (findCode)
+  #validateCode = async (code) => {
+    const products = await this.getProducts();
+    if (products.length > 0) {
+      const findCode = products.some((elem) => elem.code === code);
+      if (findCode) {
+        /* se comenta el codigo que detiene la ejecucion para cambiarlo por un console log y que se
+            ejecuten los test
         throw new Error(
           `ERROR al agregar el productos código: ${code}. El campo código no puede repetirse`
+        ); */
+        console.log(
+          `ERROR al agregar el productos código: ${code}. El campo código no puede repetirse`
         );
+        return false;
+      }
     }
+    return true;
   };
   #verifyField = (title, description, price, img, code, stock, category) => {
     if (!title || title.length === 0) {
@@ -72,29 +84,50 @@ class ProductManager {
       );
     }
   };
-  addProduct = (title, description, price, img, code, stock, category) => {
+  #saveFile = async (products) => {
+    await promises.writeFile(this.path, JSON.stringify(products));
+  };
+  addProduct = async (
+    title,
+    description,
+    price,
+    img,
+    code,
+    stock,
+    category
+  ) => {
+    const products = await this.getProducts();
     this.#verifyField(title, description, price, img, code, stock, category);
-    this.#validateCode(code);
-    const product = {
-      id: this.#generateId(),
-      title,
-      description,
-      price,
-      img,
-      code,
-      stock,
-      category,
-    };
+    const validCode = await this.#validateCode(code);
+    if (validCode) {
+      const product = {
+        id: await this.#generateId(),
+        title,
+        description,
+        price,
+        img,
+        code,
+        stock,
+        category,
+      };
 
-    this.products.push(product);
+      products.push(product);
+      return await this.#saveFile(products);
+    }
   };
 
-  getProducts = () => {
-    return this.products;
+  getProducts = async () => {
+    if (existsSync(this.path)) {
+      const productsFile = await promises.readFile(this.path, "utf8");
+      const products = JSON.parse(productsFile);
+      return products;
+    } else return [];
   };
-  getProductById = (id) => {
+
+  getProductById = async (id) => {
     this.#verifiyId(id);
-    const product = this.products.find((element) => element.id === id);
+    const products = await this.getProducts();
+    const product = products.find((element) => element.id === id);
     if (product) {
       return product;
     } else {
@@ -102,94 +135,101 @@ class ProductManager {
     }
   };
 
-  updateProduct = (product) => {
+  updateProduct = async (product) => {
     // /traigo el producto completo por si product llega sin alguna de los elementos
-    const productForUpdate = this.getProductById(product.id);
+    const products = await this.getProducts();
+    const productForUpdate = await this.getProductById(product.id);
 
     if (productForUpdate) {
-      const productIndex = this.products.findIndex(
+      const productIndex = products.findIndex(
         (element) => element.id === productForUpdate.id
       );
       const finalProduct = { ...productForUpdate, ...product };
+      products.splice(productIndex, 1, finalProduct);
 
-      return this.products.splice(productIndex, 1, finalProduct);
+      return await this.#saveFile(products);
     }
     throw new Error("ERROR. el producto no pudo ser actualizado");
   };
-  deleteProduct = (id) => {
+  deleteProduct = async (id) => {
     this.#verifiyId(id);
-    const productIndex = this.products.findIndex(
-      (element) => element.id === id
+    const products = await this.getProducts();
+    const productForDelete = products.find((element) => element.id === id);
+    const productIndex = products.findIndex((element) => element.id === id);
+    products.splice(productIndex, 1);
+    console.log(
+      `El producto ${productForDelete.title} fue eliminado correctamente`
     );
-    return this.products.splice(productIndex, 1);
+    return this.#saveFile(products);
   };
 }
 
 //------------------- TESTING
 
-const productos = new ProductManager();
-console.log(productos.getProducts());
+const productos = new ProductManager("./products.json");
 
-productos.addProduct(
-  "Cerveza IPA",
-  "Cerveza India Pale Ale, ligeramente amarga y aromática.",
-  5.99,
-  "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104302/descarga_schjmg.jpg",
-  "cervezas1001",
-  5,
-  "Cervezas"
-);
-console.log(productos.getProducts());
-productos.addProduct(
-  "Vino Tinto Reserva",
-  "Vino tinto reserva de alta calidad, cosecha 2015.",
-  12.99,
-  "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104434/CeYiCOGWSUuyzWD_JuDsOg_ytj20a.jpg",
-  "vino1001",
-  30,
-  "Vinos"
-);
-productos.addProduct(
-  "Whisky Escocés",
-  "Whisky escocés de malta, suave y ahumado.",
-  29.99,
-  "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104567/Jack-Daniels-Master-Distiller-N4-550x550_qvvkp4.png",
-  "licores1001",
-  0,
-  "Licores"
-);
-productos.addProduct(
-  "Ron Añejo",
-  "Ron añejo de 7",
-  9.99,
-  "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104541/ron-havana-club-7-anos_s9mxc4.png",
-  "licores1002",
-  25,
-  "Licores"
-);
-console.log(productos.getProducts());
-productos.getProductById(2);
-productos.updateProduct({
-  id: 3,
-  stock: 25,
-});
-productos.updateProduct({
-  id: 4,
-  description: "Ron añejo de 7 años de barrica, ideal para cócteles.",
-  price: 19.99,
-  stock: 50,
-});
-console.log(productos.getProducts());
-productos.deleteProduct(2);
-productos.addProduct(
-  "Ginebra Premium",
-  "Ginebra premium con una mezcla de botánicos.",
-  24.99,
-  "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104611/ventozelo-gin_tbmgg4.png",
-  "licores1003",
-  15,
-  "Licores"
-);
-console.log(productos.getProducts());
+(async () => {
+  console.log(await productos.getProducts());
 
-productos.getProductById(6);
+  await productos.addProduct(
+    "Cerveza IPA",
+    "Cerveza India Pale Ale, ligeramente amarga y aromática.",
+    5.99,
+    "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104302/descarga_schjmg.jpg",
+    "cervezas1001",
+    5,
+    "Cervezas"
+  );
+  console.log(await productos.getProducts());
+  await productos.addProduct(
+    "Vino Tinto Reserva",
+    "Vino tinto reserva de alta calidad, cosecha 2015.",
+    12.99,
+    "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104434/CeYiCOGWSUuyzWD_JuDsOg_ytj20a.jpg",
+    "vino1001",
+    30,
+    "Vinos"
+  );
+  await productos.addProduct(
+    "Whisky Escocés",
+    "Whisky escocés de malta, suave y ahumado.",
+    29.99,
+    "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104567/Jack-Daniels-Master-Distiller-N4-550x550_qvvkp4.png",
+    "licores1001",
+    0,
+    "Licores"
+  );
+  await productos.addProduct(
+    "Ron Añejo",
+    "Ron añejo de 7",
+    9.99,
+    "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104541/ron-havana-club-7-anos_s9mxc4.png",
+    "licores1002",
+    25,
+    "Licores"
+  );
+  console.log(await productos.getProductById(3));
+  await productos.updateProduct({
+    id: 3,
+    stock: 99,
+  });
+  await productos.updateProduct({
+    id: 4,
+    description: "Ron añejo de 7 años de barrica, ideal para cócteles.",
+    price: 19.99,
+    stock: 50,
+  });
+  console.log(await productos.getProducts());
+  await productos.deleteProduct(6);
+  await productos.addProduct(
+    "Ginebra Premium",
+    "Ginebra premium con una mezcla de botánicos.",
+    24.99,
+    "https://res.cloudinary.com/dg8ndxl2y/image/upload/v1696104611/ventozelo-gin_tbmgg4.png",
+    "licores1003",
+    15,
+    "Licores"
+  );
+  console.log(await productos.getProducts());
+  //console.log(await productos.getProductById(7));
+})();
