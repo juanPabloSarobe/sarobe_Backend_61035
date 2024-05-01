05f01b498b9d96af5b0fb47bc9e1fd70b5dd7c5c
juanPabloSarobe <jp.sarobe@gmail.com>
2024-05-01 15:04:24 -0300

    metodo static implementado

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import productRouter from "./routes/products.router.js ";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname + "/public");
 const app = express();
 
-app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+/* app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/products", productRouter);
+app.use("/products", productRouter); */
 
 const PORT = 8080;
 
